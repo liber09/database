42e2ea494c7f325ac7f918d75eb79f7f1381c214
liber09 <linda.bergsangel@hotmail.com>
2022-12-20 05:08:17 +0100

    Tables for order and product image

--- CreateTables.sql
+++ CreateTables.sql
@@ -11,5 +11,26 @@ CREATE TABLE Products (
     ID int NOT NULL PRIMARY KEY,
     Name varchar(255) NOT NULL,
     Description varchar(255),
-	
+	Price decimal,
 );
+
+CREATE TABLE ProductImages (
+    ID int NOT NULL PRIMARY KEY,
+	ProductId int ,
+    Name varchar(255) NOT NULL,
+    Image varbinary(MAX)
+);
+
+
+CREATE TABLE ORDERS (
+    ID int NOT NULL PRIMARY KEY,
+    CustomerID int NOT NULL,
+	TotalAmount decimal,
+	OrderStatus int
+);
+
+CREATE TABLE OrderRows(
+	ID int NOT NULL PRIMARY KEY,
+	OrderId int NOT NULL,
+	ProductId int NOT NULL,
+
