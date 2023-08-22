63d38071f971084e714f5350c9d7bcf5a6736502
liber09 <113246195+liber09@users.noreply.github.com>
2023-08-22 20:14:36 +0200

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,7 +1,7 @@
 --CRUD
 
 --Create
---Användare 1 lägger en beställning på 1 st av produkt 1
+--User 1 places a order on 1 of product 1
 INSERT INTO webshop.dbo.Orders VALUES (1,'2022-12-14 15:12:33',199.90,1);
 INSERT INTO webshop.dbo.OrderRows VALUES (5,1,1);
 
@@ -12,7 +12,7 @@ INNER JOIN OrderRows On orders.id = OrderRows.OrderId
 Where Orders.id = 5
 
 --Update
---Användaren ändrar sin order och beställer två stycken produkter istället
+--The user changes its order and orders two products instead
 Begin transaction
 UPDATE Orders
 SET Orders.TotalAmount = 398.80
@@ -30,11 +30,9 @@ WHERE Orders.Id = 5
 
 Commit 
 
-
---2 valfria mattefunktioner
---Lista alla ordrar med datum 1 december 2022 eler senare
+--2 free chosen mathFunctions
+--List all orders with date from 1 december 2022 or later
 SELECT * FROM Orders WHERE OrderDate > '2022-11-30'
 
-
---Vilket order id har den dyraste beställningen
+--What order id has the most expensive order
 SELECT TOP (1) Id FROM Orders order by TotalAmount desc
